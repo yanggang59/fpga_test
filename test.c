78c4b38c58176105f459593ef9c88417187b24dd
yanggang <1449381582@qq.com>
2023-10-11 19:02:20 +0800

    modify producer and consumer

--- test.c
+++ test.c
@@ -10,6 +10,7 @@
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t producer_ready = PTHREAD_COND_INITIALIZER;
+pthread_cond_t consumer_ready = PTHREAD_COND_INITIALIZER;
 
 int g_running_cycles = 0;
 void* g_mem = NULL;
@@ -22,18 +23,21 @@ void* producer_task(void* arg)
     int* consumer_ref = producer_ref + 1;
     int producer_val_bak = 0;
     int consumer_val_bak = 0;
-    printf("[DEBUG] producer running 1\r\n");
     *producer_ref = 0;
     while(1) {
         printf("[DEBUG] producer running \r\n");
-        if(++g_running_cycles > 1000000) break;
+        if(++g_running_cycles > 1000) break;
+        printf("[DEBUG] producer tring to lock \r\n");
         pthread_mutex_lock(&mutex);
+        printf("[DEBUG] producer lock , consumer = %d, consumer_bak = %d\r\n", *consumer_ref, consumer_val_bak);
         while(*consumer_ref != consumer_val_bak + 1);
         consumer_val_bak = *consumer_ref;
         producer_val_bak = *producer_ref;
         *producer_ref = producer_val_bak + 1;
+        printf("[DEBUG] producer unlock \r\n");
         pthread_mutex_unlock(&mutex);
-        //pthread_cond_signal(&producer_ready);
+        pthread_cond_signal(&producer_ready);
+        pthread_cond_wait(&consumer_ready, &mutex);
     }
     printf("[DEBUG] producer left \r\n");
 }
@@ -47,7 +51,6 @@ void* consumer_task(void* arg)
     int* consumer_ref = producer_ref + 1;
     int producer_val_bak = 0;
     int consumer_val_bak = 0;
-    printf("[DEBUG] consumer running 1\r\n");
     /**
     * Need to set consumer as 1, so producer can start first
     */
@@ -55,11 +58,18 @@ void* consumer_task(void* arg)
     while(1) {
         printf("[DEBUG] consumer running \r\n");
         if(g_running_cycles > 1000) break;
+        printf("[DEBUG] consumer wait for condition , consumer = %d, consumer_bak = %d\r\n", *consumer_ref, consumer_val_bak);
+        pthread_cond_wait(&producer_ready, &mutex);
+        printf("[DEBUG] consumer tring to lock \r\n");
         pthread_mutex_lock(&mutex);
+        printf("[DEBUG] consumer lock , producer = %d, producer_bak = %d\r\n", *consumer_ref, consumer_val_bak);
         while(*producer_ref != producer_val_bak + 1);
         producer_val_bak = *producer_ref;
-        //pthread_cond_wait(&producer_ready, &mutex);
+        consumer_val_bak = *consumer_ref;
+        *consumer_ref = consumer_val_bak + 1;
+        printf("[DEBUG] consumer unlock \r\n");
         pthread_mutex_unlock(&mutex);
+        pthread_cond_signal(&consumer_ready);
     }
     printf("[DEBUG] consumer left \r\n");
 }
@@ -70,7 +80,7 @@ int main()
     int err = 0;
     g_mem = malloc(1024);
 
-    printf("Creating Producer Task \r\n");
+    printf("Creating Tasks \r\n");
 
     err = pthread_create(&thread_producer, NULL, producer_task, NULL);
     if (err != 0) {
@@ -78,13 +88,11 @@ int main()
         return err;
     }
 
-    printf("Creating Consumer Task \r\n");
     err = pthread_create(&thread_consumer, NULL, consumer_task, NULL);
-    if (err != 0)
+    if (err != 0) {
         printf("[Error] can't create consumer thread");
         return err;
-
-    printf("Deleting Tasks \r\n");
+    }
 
     pthread_join(thread_producer, NULL);
     pthread_join(thread_consumer, NULL);
