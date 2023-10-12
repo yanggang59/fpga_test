ea2fec1bc746568adc98d1c1b0dfe2980d14691b
yanggang <1449381582@qq.com>
2023-10-12 11:34:24 +0800

    add running limits

--- test.c
+++ test.c
@@ -16,18 +16,21 @@ int g_running_cycles = 0;
 void* g_mem = NULL;
 volatile int g_init = 0;
 
+#define RUNNING_CYCLE_LIMITS     1000000
+
 void* task1(void* arg)
 {
     int* val1_ref = g_mem;
     int* val2_ref = val1_ref + 1;
-
     volatile int val1;
     volatile int val2;
+
+    int g_count = 0;
     while(1)
 	{
 		pthread_mutex_lock(&mutex);
         if(!g_init) {
-            printf("[DEBUG] Task 1 Init \r\n");
+            printf("[DEBUG] Task 1 do init \r\n");
             g_init = 1;
             val1 = *val1_ref = 0;
             val2 = 0;
@@ -35,14 +38,15 @@ void* task1(void* arg)
         } else {
             val2 = *val2_ref -1;
         }
-        /**
-        * inc area 1
-        */
         val1 = *val1_ref = val1 + 1;
-		printf("[Info] Thread 1 , val1_ref = %d, val1 = %d, val2_ref = %d, val2 = %d \n", *val1_ref, val1, *val2_ref, val2);
+		//printf("[Info] Thread 1 , val1_ref = %d, val1 = %d, val2_ref = %d, val2 = %d \n", *val1_ref, val1, *val2_ref, val2);
         while(*val2_ref != val2 + 1);
         val2 = *val2_ref;
 		pthread_cond_signal(&cond2);
+        if(++g_count > RUNNING_CYCLE_LIMITS) {
+            pthread_mutex_unlock(&mutex);
+            break;
+        }
 		pthread_cond_wait(&cond1,&mutex);
 		pthread_mutex_unlock(&mutex);
 	}
@@ -57,10 +61,13 @@ void* task2(void* arg)
     int* val2_ref = val1_ref + 1;
     volatile int val2;
     volatile int val1;
+    
+    int g_count = 0;
+
     while(1){
 		pthread_mutex_lock(&mutex);
         if(!g_init) {
-            printf("[DEBUG] Task 2 Init \r\n");
+            printf("[DEBUG] Task 2 do init \r\n");
             g_init = 1;
             val2 = *val2_ref = 0;
             val1 = 0;
@@ -68,12 +75,15 @@ void* task2(void* arg)
         } else {
             val1 = *val1_ref -1;
         }
-        //sleep(1);
 		val2 = *val2_ref = val2 + 1;
-		printf("[Info] Thread 2 , val1_ref = %d, val1 = %d, val2_ref = %d, val2 = %d \n", *val1_ref, val1, *val2_ref, val2);
+		//printf("[Info] Thread 2 , val1_ref = %d, val1 = %d, val2_ref = %d, val2 = %d \n", *val1_ref, val1, *val2_ref, val2);
         while(*val1_ref != val1 + 1);
         val1 = *val1_ref;
 		pthread_cond_signal(&cond1);
+        if(++g_count > RUNNING_CYCLE_LIMITS) {
+            pthread_mutex_unlock(&mutex);
+            break;
+        }
 		pthread_cond_wait(&cond2,&mutex);
 		pthread_mutex_unlock(&mutex);
 	}
@@ -92,13 +102,13 @@ int main()
 
     err = pthread_create(&thread1, NULL, task1, NULL);
     if (err != 0) {
-        printf("[Error] can't create producer thread");
+        printf("[Error] can't create thread1");
         return err;
     }
 
     err = pthread_create(&thread2, NULL, task2, NULL);
     if (err != 0) {
-        printf("[Error] can't create consumer thread");
+        printf("[Error] can't create thread2");
         return err;
     }
 
